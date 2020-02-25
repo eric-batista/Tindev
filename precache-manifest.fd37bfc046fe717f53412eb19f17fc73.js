e94e0824bb7e740a5e554dc9cee3f844a4bb46dd
Eric Batista <klose.eric31@gmail.com>
2020-02-24 22:08:44 -0300

    Updates

---
+++ precache-manifest.fd37bfc046fe717f53412eb19f17fc73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d460790bc1f6dc6a2267d6bc7abbfd0",
+    "url": "/Tindev/index.html"
+  },
+  {
+    "revision": "dafbe1f4682ffec67855",
+    "url": "/Tindev/static/css/main.95bc63f3.chunk.css"
+  },
+  {
+    "revision": "09b6f8522c8d33426615",
+    "url": "/Tindev/static/js/2.6f6adfef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Tindev/static/js/2.6f6adfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafbe1f4682ffec67855",
+    "url": "/Tindev/static/js/main.0ba58478.chunk.js"
+  },
+  {
+    "revision": "848ac69948244f0c5427",
+    "url": "/Tindev/static/js/runtime-main.74c207af.js"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/Tindev/static/media/logo.6a01ceef.svg"
+  }
+]);

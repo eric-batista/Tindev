dae4ee1d63cfdd70e437aab0bdecdf243e5419bd
Eric Batista <klose.eric31@gmail.com>
2020-02-24 22:09:37 -0300

    gh-pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 
 export default function Routes() {
   return(
-    <BrowserRouter>
-      <Route path="/" exact component={Login}/>
-      <Route path="/main" component={Main}/>
-    </BrowserRouter>
+    <HashRouter>
+      <Route path="/Tindev/" exact component={Login}/>
+      <Route path="/Tindev/main" component={Main}/>
+    </HashRouter>
   );
 }
